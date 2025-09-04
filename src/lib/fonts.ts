fbfcdb13293070e8a2a310b5039517475aa6d3a5
adityanandanx <nandanaditya985@gmail.com>
2025-09-05 03:43:03 +0530

    chore: fix fonts preloading

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -2,12 +2,15 @@ import { Bodoni_Moda_SC, Space_Grotesk, Space_Mono } from "next/font/google";
 
 export const fontSans = Space_Grotesk({
   variable: "--font-sans",
+  subsets: ["latin"],
 });
 
 export const fontSerif = Bodoni_Moda_SC({
   variable: "--font-serif",
+  subsets: ["latin"],
 });
 export const fontMono = Space_Mono({
   variable: "--font-mono",
+  subsets: ["latin"],
   weight: ["400"],
 });
